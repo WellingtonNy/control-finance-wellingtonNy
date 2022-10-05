cc9da87adef0a15938b790c72bfa3b2654df47bd
Wellington <Wellingtoncpfilho@gmail.com>
2022-10-04 23:08:36 -0300

    add modal e Li com DOM apartir do array de objeto

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,42 @@
-/* Desenvolva sua lógica aqui */
+function criarCard(objeto) {
+    
+let li = document.createElement('li')
+let lidiv= document.createElement('div')
+let lidiv2= document.createElement('div')
+let lispan= document.createElement('span')
+let libot= document.createElement('button')
+let liimg= document.createElement('img')
+
+li.classList= 'card'
+lispan.classList='tag'
+libot.classList='lixeira'
+
+lidiv.innerText=`R$:${objeto.value}`
+if (objeto.categoryID === 1) {
+    lispan.innerText='saida'
+}else{
+lispan.innerText= 'Entrada'
+}
+liimg.src="/assets/trash.png"
+
+
+
+li.append(lidiv, lidiv2)
+lidiv2.append(lispan, libot)
+libot.append(liimg)
+
+return li
+}
+
+function criador(elemento) {
+   let ul = document.querySelector('ul')
+let li = criarCard(elemento)
+ ul.appendChild(li)
+}
+
+function gerador(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        criador(arr[i])    
+    }  
+}
+gerador(insertedValues)
